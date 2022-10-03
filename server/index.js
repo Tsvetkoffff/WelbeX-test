151c15c9ff458d00846ad49ca2b9b87d32daa929
Slava Tsvetkoffff <v.tsvetkoffff@gmail.com>
2022-10-03 08:12:25 +0300

    fix: add Access-Control-Allow-Origin to avoid CORS

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(express.json())
+
 app.use('/api', router)
 
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`))
